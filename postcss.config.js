b7255fa3e967dd25c300b320512a09c1f769060c
Kevin Adam <rexprimeadam@gmail.com>
2020-04-14 16:16:24 +0700

    fix npm script

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
+const tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
-        // ...
-        require('tailwindcss'),
-        require('autoprefixer'),
-        // ...
-    ]
-}
+        tailwindcss('./tailwind.config.js'),
+        require('autoprefixer')
+    ],
+};
